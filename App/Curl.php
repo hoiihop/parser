1cd38e06b7a905c8bf5707c5ba6c753bc3eb0193
Maxim Salij <max.salej@gmail.com>
2017-10-03 15:41:01 +0300

    + test

---
+++ App/Curl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maxim
+ * Date: 03.10.2017
+ * Time: 15:38
+ */
+
+namespace App;
+
+
+class Curl
+{
+
+}
---
+++ App/Parser.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maxim
+ * Date: 03.10.2017
+ * Time: 15:39
+ */
+
+namespace App;
+
+
+class Parser
+{
+
+}
